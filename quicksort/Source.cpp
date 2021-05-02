f93d902ce70dbc7711fa5839d52a0516ce38662e
lamngocchien <lamngocchien@gmail.com>
2021-05-02 09:50:45 +0700

    final

--- quicksort/Source.cpp
+++ quicksort/Source.cpp
@@ -1,11 +1,57 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
 #include<iostream>
 using namespace std;
 
-
-int main(){
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-
-	cout << "10"<< endl;
-	return 0;
+// A utility function to swap two elements
+void swap(int* a, int* b)
+{
+    int t = *a;
+    *a = *b;
+    *b = t;
 }
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int index = low;
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            swap(&arr[j], &arr[index]);
+            index++;
+        }
+    }
+
+    swap(&arr[index], &arr[high]);
+
+    return index;
+}
+
+void quicksort(int arr[], int low, int high){
+    if (low < high) {
+        int pivot = partition(arr, low, high);
+        quicksort(arr, low, pivot - 1);
+        quicksort(arr, pivot+1, high);
+    }
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver Code
+int main()
+{
+    int arr[] = { 10, 7, 8, 9, 1, 5, 4, 2, 3, 0, 5, 10, 0 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quicksort(arr, 0, n - 1);
+    cout << "Sorted array: \n";
+    printArray(arr, n);
+    return 0;
+}
+
+// This code is contributed by rathbhupendra
